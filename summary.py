4cb038f65c4201ebe01f6702c1e96713051da3d3
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2022-01-22 01:55:11 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     m       = YoloBody([[6, 7, 8], [3, 4, 5], [0, 1, 2]], 80, 'l').to(device)
 
-    summary(m, input_size=(3, 416, 416))
+    summary(m, input_size=(3, 640, 640))
